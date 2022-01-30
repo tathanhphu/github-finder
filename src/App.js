3ea91d982b4317ecc7269a30b4aea5ff66833b37
phuta.local <tathanhphu@gmail.com>
2022-01-30 13:42:47 +0700

    react route, added route /user/:login, route /about

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-
 import React, {Component} from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users'
+
 import axios from 'axios';
-import Search from './components/users/Search';
 import { Alert } from './components/layout/Alert';
+import { About } from './components/pages/About';
+import Home from './components/pages/Home';
+import User from './components/users/User';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   }
@@ -20,6 +22,15 @@ class App extends Component {
     &client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
     client_secrect=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     this.setState({loading: false, users: res.data.items});
+  }
+  // Get a single Github user
+  getUser = async(username) => {
+    this.setState({ loading: true});
+    const res = await axios.get(`https://api.github.com/users/${username}
+    ?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+    &client_secrect=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({loading: false, user: res.data});
+
   }
   // Clear users from state
   clearUsers = () => {
@@ -40,20 +51,29 @@ class App extends Component {
   //   this.setState({loading: false, users: res.data});
   // }
   render() {
-    const {users, loading} = this.state;
+    const {users, loading, user} = this.state;
     return (
+      <Router>
       <div className="App">
         <Navbar/>
         <div className='container'>
           <Alert alert={this.state.alert}/>
-          <Search 
-          searchUsers={this.searchUsers} 
-          clearUsers={this.clearUsers} 
-          showClear={users.length > 0 ? true: false}
-          setAlert={this.setAlert}/>
-          <Users loading={loading} users={users}/>
+          <Routes>
+            
+            <Route exact path='/about' element={<About/>} />
+            
+            <Route 
+              exact path='/' 
+              element={<Home searchUsers={this.searchUsers} 
+              setAlert={this.setAlert}
+              clearUsers={this.clearUsers} users={users} loading={loading}/>} />
+            
+            <Route path='/user/:login'  element={<User getUser={this.getUser} loading={loading} user={user} {...this.props}/>} />
+          </Routes>
+          
         </div>
       </div>
+      </Router>
     );
   } 
 }
